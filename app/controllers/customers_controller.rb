29ab446f426fb19abf6c068ab8148af55dfb661b
Sergio Oseko <osk.sergio@gmail.com>
2022-06-30 23:36:56 -0300

    feat: edit and update methods implemented (customer)

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,8 +1,13 @@
 class CustomersController < ApplicationController
+  before_action :set_customer, only: %i[ edit update ]
 
-  # Get /customers/new
+  # GET /customers/new
   def new
     @customer = Customer.new
+  end
+
+  # GET /customers/1/edit
+  def edit
   end
 
   # POST /customers or  /customers.json
@@ -19,7 +24,24 @@ class CustomersController < ApplicationController
     end
   end
 
+  # PATH/PUT /customers/1 or /customers/1.json
+  def update
+    respond_to do |format|
+      if @customer.update(customer_params)
+        format.html { redirect_to customer_url(@customer), notice: "O cliente foi atualizado com sucesso." }
+        format.json { render :show, status: :ok, location: @customer }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @customer.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
+
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
 
   def customer_params
     params.require(:customer).permit(:description, :fantasy_name, :document, :another_document, :zip_code,
